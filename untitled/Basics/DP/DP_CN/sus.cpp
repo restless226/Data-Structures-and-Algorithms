2f447fe7d75160734d1cda6b96e97df9f7d763f7
iamrohit <rohit@gmail.com>
2021-09-19 10:00:06 +0530

    -

---
+++ untitled/Basics/DP/DP_CN/sus.cpp
@@ -0,0 +1,28 @@
+///*
+//Problem Description:
+//Given two strings s and t.
+//Find the length shortest subsequence in s such that it is not a subsequence in t.
+//*/
+//
+//#include <bits/stdc++.h>
+//
+//#define int long long
+//using namespace std;
+//
+//void solve() {
+//}
+//
+//int32_t main() {
+//    cin.tie(NULL);
+//    cout.tie(NULL);
+//    ios_base::sync_with_stdio(false);
+//
+//    int t;
+//    cin >> t;
+//
+//    while (t--) {
+//        solve();
+//    }
+//
+//    return 0;
+//}
