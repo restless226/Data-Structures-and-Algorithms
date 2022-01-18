b1297dc267a6b471af66e1ed6c1de5c0a5fb66ed
iamrohit <rohit@gmail.com>
2022-01-18 08:53:24 +0530

    Algorithmic Toolbox

--- untitled/algorithmic_toolbox/sum1.cpp
+++ untitled/algorithmic_toolbox/sum1.cpp
@@ -1,23 +1,16 @@
-//#include <iostream>
-//using namespace std;
-//int main(){
-//    int a,b;
-//    cin>>a;
-//    cin>>b;
-//    cout<<a+b;
-//}
+#include <iostream>
 
-//#include <iostream>
-//
-//int sum_of_two_digits(int first_digit, int second_digit) {
-//    return first_digit + second_digit;
-//}
-//
-//int main() {
-//    int a = 0;
-//    int b = 0;
-//    std::cin >> a;
-//    std::cin >> b;
-//    std::cout << sum_of_two_digits(a, b);
-//    return 0;
-//}
+using namespace std;
+
+int sum_of_two_digits(int first_digit, int second_digit) {
+    return first_digit + second_digit;
+}
+
+int main() {
+    int a = 0;
+    int b = 0;
+    std::cin >> a;
+    std::cin >> b;
+    std::cout << sum_of_two_digits(a, b);
+    return 0;
+}
--- untitled/algorithmic_toolbox/test.cpp
+++ untitled/algorithmic_toolbox/test.cpp
@@ -1,64 +1,37 @@
-//#include <iostream>
-//#include <vector>
-//#include <algorithm>
-//using namespace std;
-//
-////long long MaxPairwiseProduct(const std::vector<int> &numbers) {
-////    long long max_product = 0;
-////    int n = numbers.size();
-////    for (int first = 0; first < n; ++first) {
-////        for (int second = first + 1; second < n; ++second) {
-////            if ((long long) numbers[first] * numbers[second] > max_product) {
-////                max_product = (long long) numbers[first] * numbers[second];
-////            }
-////        }
-////    }
-////
-////    return max_product;
-////}
-//
-//long long MaxPairwiseProductFast(const vector<int>& numbers) {
-//    int n = numbers.size();
-//
-//    int max_index1 = -1;
-//    for (int i = 0; i < n; ++i) {
-//        if (max_index1 == -1 || (numbers[i] > numbers[max_index1])) {
-//            max_index1 = i;
-//        }
-//    }
-//
-//    int max_index2 = -1;
-//    for (int j = 0; j < n; ++j) {
-//        if (j!=max_index1 && (max_index2==-1 || numbers[j] > numbers[max_index2])) {
-//            max_index2 = j;
-//        }
-//    }
-//
-//    return ((long long) (numbers[max_index1])) * numbers[max_index2];
-//}
-//
-//int main() {
-////    while(true){
-////        int n=rand()%10 +2;
-////        cout<<n<<"\n";
-////        vector<int> a;
-////        for(int i=0;i<n;++i){
-////            a.push_back(rand()%100000);
-////        }
-////        for(int i=0;i<n;++i){
-////            cout<<a[i]<<' '<<endl;
-////        }
-////    }
-////    cout<<"\n";
-//
-//    int n;
-//    cin >> n;
-//    vector<int> numbers(n);
-//    for (int i = 0; i < n; ++i) {
-//       cin >> numbers[i];
-//    }
-//
-//    long long result = MaxPairwiseProductFast(numbers);
-//    cout << result << "\n";
-//    return 0;
-//}
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+long long MaxPairwiseProductFast(const vector<int> &numbers) {
+    int n = numbers.size();
+
+    int max_index1 = -1;
+    for (int i = 0; i < n; ++i) {
+        if (max_index1 == -1 || (numbers[i] > numbers[max_index1])) {
+            max_index1 = i;
+        }
+    }
+
+    int max_index2 = -1;
+    for (int j = 0; j < n; ++j) {
+        if (j != max_index1 && (max_index2 == -1 || numbers[j] > numbers[max_index2])) {
+            max_index2 = j;
+        }
+    }
+
+    return ((long long) (numbers[max_index1])) * numbers[max_index2];
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> numbers(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> numbers[i];
+    }
+
+    long long result = MaxPairwiseProductFast(numbers);
+    cout << result << "\n";
+    return 0;
+}
